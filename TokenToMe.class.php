fda55b060a8d9a982d425b17809d74220b82b017
TweetPressFr <tweetpressfr@gmail.com>
2014-06-26 01:47:44 +0200

    first commit

---
+++ TokenToMe.class.php
@@ -0,0 +1,48 @@
+<?php
+
+if( ! class_exists('TokenToMe') ) {	
+	
+	class TokenToMe{
+
+		public $consumer_key;
+		protected $consumer_secret;
+
+		public function __construct( $consumer_key, $consumer_secret ) 
+		{
+
+			$this->consumer_key 	= $consumer_key;
+			$this->consumer_secret  = $consumer_secret;
+			
+		}
+
+
+		public function get_access_token()
+		{
+
+			$credentials = $this->consumer_key . ':' . $this->consumer_secret;
+			$auth        = base64_encode($credentials);
+
+
+			$params = array(
+				'method' 		=> 'POST',
+				'httpversion' 	=> '1.1',
+				'blocking' 		=> true,
+				'headers' 		=> array(
+					'Authorization' => 'Basic ' . $auth,
+					'Content-Type' 	=> 'application/x-www-form-urlencoded;charset=UTF-8'// !important
+				),
+				
+				'body' 			=> array( 'grant_type' => 'client_credentials' )
+			);
+
+			$call 	  = wp_remote_retrieve_body( wp_remote_post('https://api.twitter.com/oauth2/token', $params) );
+			$keys     = json_decode($call);
+
+			if( $keys && !is_null( $keys ) ) $access_token = $keys->access_token;
+
+			return $access_token;
+			
+		}
+
+	}
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# TokenToMe #
+
+Get access token from Twitter
+
+## Requirements ##
+
+consumer key and consumer secret : <a href="https://apps.twitter.com/app/new">Get yours !</a>
+
+
+## Description ##
+
+Class that allows you to grab an access token from Twitter in WordPress
+
+## Changelog ##
+
+# 1.0
+* First commit
